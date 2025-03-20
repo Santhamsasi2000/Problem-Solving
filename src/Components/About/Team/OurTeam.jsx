6cb469c656c8dc2e01d1f308af4a7255d4740f27
Sasikumar <santhamsasi68670@gmail.com>
2025-03-20 11:49:39 +0530

    New Changes

--- src/Components/About/Team/OurTeam.jsx
+++ src/Components/About/Team/OurTeam.jsx
@@ -10,7 +10,7 @@ const OurTeam = () => {
             DataOurTeam.map(({id,name,designation})=>(
             <div className="col-sm-6 col-md-4 col-lg-3">
               <div className="important-component card p-0 border-0 h-100" >
-                <img className="card-img-top" src="public\Images\AboutUs\Team\Employee Image - Problem Solving Consultancy - Best Business Consultancy- Coimbatore.webp" alt="No-Image" />
+                <img className="card-img-top" src="/Images/AboutUs/Team/Employee Image - Problem Solving Consultancy - Best Business Consultancy- Coimbatore.webp" alt="No-Image" />
                 <div className="card-body">
                   <p className="subtitle text-center mb-2">{name}</p>
                   <p className="card-subtitle text-center">{designation}</p>
--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,6 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
-import CaseStudies from './Components/CaseStudies/CaseStudies.jsx'
-import Services from './Components/Services/Services.jsx'
-import Careers from './Components/About/Career/Careers.jsx'
-import Home from './Components/Home/Home.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
